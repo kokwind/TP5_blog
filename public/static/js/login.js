9980fe8a48420d4e5c62fa75032fc499a125b00f
kokwind <452599544@qq.com>
2017-10-26 21:49:47 +0800

    添加statci文件夹

---
+++ public/static/js/login.js
@@ -0,0 +1,15 @@
+function login() {  
+    
+      var username = document.getElementById("username");  
+      var pass = document.getElementById("password");  
+    
+      if (username.value == "") {  
+    
+          alert("请输入用户名");  
+    
+      } else if (pass.value  == "") {  
+    
+          alert("请输入密码");  
+    
+      }
+  }  
