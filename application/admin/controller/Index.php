d7758c78967291da9dc27d53a830b6b294eead1a
kokwind <452599544@qq.com>
2017-10-26 11:32:56 +0800

    使用模板继承

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -8,26 +8,6 @@ class Index extends Controller
     //进入admin页面
     public function Index()
     {
-        
-        return $this->fetch();
-    }
-
-    //后台页面头部
-    public function Top()
-    {
-        
-        return $this->fetch();
-    }
-
-    //左侧导航menu
-    public function Menu()
-    {
-        return $this->fetch();
-    }
-
-    //右侧展示界面
-    public function Desk()
-    {
         $index_model = new AdminIndex;
         $data = $index_model->getDeskData();
         
@@ -37,12 +17,7 @@ class Index extends Controller
         $this->assign('hide_article',$data['hide_article']);
         $this->assign('all_comment',$data['all_comment']);
         //渲染模板
-        return $this->fetch();
+        return $this->fetch("Index/index");
     }
 
-    //底部bottom
-    public function Bottom()
-    {
-        return $this->fetch();
-    }
 }
